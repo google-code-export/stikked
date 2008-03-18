936ff9f1ec610bc926f0f5d4e43b16b9cd6dfa78
ben@stikked.com <ben@stikked.com@3a42ef3b-d73e-0410-944d-ff45a74ca95b>
2008-03-18 20:48:10 +0000

    Creating a branch for stikked 0.4

---
+++ branches/stikked-0.4/system/application/views/view.php
@@ -0,0 +1,34 @@
+<?php $this->load->view('header', $data); ?>
+<?php if(isset($insert)){
+	echo $insert;
+}?>
+
+<script type="text/javascript">
+$(document).ready(function(){
+		$(".raw").hide();
+		$(".rawl a").click(function() { 
+	      $('.raw').fadeIn(); 
+	    });
+});
+</script>
+
+<div class="paste">
+	<div class="info">
+		<h1 class="pagetitle right"><?=$title?></h1>
+		<div class="meta">
+			<span class="detail"><strong>By</strong> <?=$name?>, <? $p = explode(',', timespan($created, time())); echo $p[0]?> ago, written in <?=$lang?>.</span><br/>
+			<span class="detail"><strong>URL </strong><a href="<?=$url?>"><?=$url?></a></span><br/>
+			<span class="detail rawl"><strong><a href="#">View Raw</a></strong></span>
+		</div>
+	</div>
+	
+	<div class="raw">
+		<textarea rows="auto" cols="auto"><?=$raw?></textarea>
+	</div>
+	
+	<div class="text_formatted">
+		<?=$paste?>
+	</div>
+</div>
+
+<?php $this->load->view('footer'); ?>
